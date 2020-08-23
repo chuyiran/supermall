042ad71a87171ac97e70bd19d5ca14053d61ee68
储怡然 <252964202@qq.com>
2020-08-23 15:41:57 +0800

    配置别名

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <template>
-  <div id="app">
-  </div>
+  <div id="app"></div>
 </template>
 
 <script>
+//引入全局js
 export default {
-  name:'app',
-  components:{}
-}
+  name: "app",
+  components: {},
+};
 </script>
 <style>
+  @import "assets/css/base.css";
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+//引入移动端适配插件
+import  'lib-flexible'
 
 Vue.config.productionTip = false
 
